e56bc20958ac9f82f526984fc2741fe4e715267b
emilyncp <107952288+emilyncp@users.noreply.github.com>
2022-07-06 14:59:29 +0530

    Add files via upload

---
+++ 9/ex9.rb
@@ -0,0 +1,8 @@
+day = "mon tue wed thu fri sat sun"
+month = "jan\nfeb\nmar\napr\nmay\njun\njul\naug\nsep\noct\nnov\ndec"
+puts "the days #{day}"
+puts "the moths#{month}"
+puts %q{
+something.
+new in.
+3 line.}
