3dcd102a71c6c35b4b6d22db532a06c4bfa29158
emilyncp <107952288+emilyncp@users.noreply.github.com>
2022-07-06 14:55:19 +0530

    Add files via upload

---
+++ 7/ex7.rb
@@ -0,0 +1,14 @@
+puts "once up on a time."
+puts "there was a #{'women'}"
+puts "who was very beautiful"
+puts "-"*10
+a = "f"
+b = "o"
+c = "o"
+d = "t"
+e = "b"
+f = "a"
+g = "l"
+h = "l"
+print a + b + c + d
+puts e + f + g + h
