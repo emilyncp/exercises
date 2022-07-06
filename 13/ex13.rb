5034aafb89f04e7924a0748da281548c70581daa
emilyncp <107952288+emilyncp@users.noreply.github.com>
2022-07-06 16:53:53 +0530

    Add files via upload

---
+++ 13/ex13.rb
@@ -0,0 +1,8 @@
+one, two, three,four= ARGV
+puts "first variable is #{one}"
+puts "second variable is #{two}"
+puts "third variable is #{three}"
+puts "fourth variable is #{four}"
+puts "select one"
+select = $stdin.gets.chomp
+puts select
