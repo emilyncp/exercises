6bb5cad06ade502d291088bb459dcffdbe2878a6
emilyncp <107952288+emilyncp@users.noreply.github.com>
2022-07-06 15:06:41 +0530

    Add files via upload

---
+++ 12/ex12.rb
@@ -0,0 +1,14 @@
+print "number:"
+num = gets.chomp.to_i
+x = num*10
+puts "x is #{x}"
+print "number:"
+dig = gets.chomp
+num = dig.to_i
+y = num/100
+puts "y is #{y}"
+print "number"
+num = gets.chomp.to_f
+z = num/10
+print "10% of "
+puts z
