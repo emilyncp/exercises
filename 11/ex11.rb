4bd1be13dd38b67e903006c6ee245c2950eaba83
emilyncp <107952288+emilyncp@users.noreply.github.com>
2022-07-06 15:03:27 +0530

    Add files via upload

---
+++ 11/ex11.rb
@@ -0,0 +1,14 @@
+print "Age"
+age = gets.chomp
+print "Height"
+height = gets.chomp
+print "weight"
+weight = gets.chomp
+puts "Age:#{age}   Height:#{height}   weight:#{weight}"
+print "X="
+x = gets.chomp.to_i
+print "Y="
+y = gets.chomp.to_i
+print "X+Y=#{x+y}"
+puts "#{x.class}"
+puts "ehbsnj"
