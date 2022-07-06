015b3a7723d0e95bc51b706daa641551fe7cfd30
emilyncp <107952288+emilyncp@users.noreply.github.com>
2022-07-06 14:54:00 +0530

    Add files via upload

---
+++ 6/ex6.rb
@@ -0,0 +1,15 @@
+people = 20
+x = "there are #{people} types of people"
+b = "binary"
+d = "don't"
+y = "those who know #{b} and those who #{d}"
+puts x
+puts y
+puts "1. #{x}"
+puts "2. #{y}"
+hil = false
+jok_valuation = "Isn't that joke so funny?! #{hil}"
+puts jok_valuation
+w= "this is left....."
+e= "this is right"
+puts w + e
