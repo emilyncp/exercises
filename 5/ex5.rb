49d6da207ea5c6bc8613816251b5ff08cb1796bd
emilyncp <107952288+emilyncp@users.noreply.github.com>
2022-07-06 14:52:21 +0530

    Add files via upload

---
+++ 5/ex5.rb
@@ -0,0 +1,15 @@
+name = 'Zed A. Shaw'
+age = 35
+height =74 #inches
+weight = 180 #lbs
+eyes = 'blue'
+teeth = 'white'
+hair = 'brown'
+
+puts "let's talk about #{name}"
+puts "he's #{height} inches tall"
+puts "he's #{weight} pounds heavy"
+puts "he's got #{eyes} eyes and #{hair} hair"
+puts "his teeth are usualy #{teeth}"
+
+puts "if i add #{age},#{height}, and #{weight} i get #{age + height +weight}"
