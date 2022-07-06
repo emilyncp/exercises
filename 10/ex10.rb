eec8999ced2a0bf936e3f6e2547493d41d640e1f
emilyncp <107952288+emilyncp@users.noreply.github.com>
2022-07-06 15:00:48 +0530

    Add files via upload

---
+++ 10/ex10.rb
@@ -0,0 +1,17 @@
+x = "\tI'm tabbed"
+y = "I'm split\nline"
+z = "I'm \\ a \\ backlash"
+li = """
+list:\n\t*apple
+\t*mango
+\t*gua
+\t*chikkt\bu
+"""
+puts x
+puts y
+puts z
+puts li
+puts "one \fexample"
+puts "\vanother  example of fo\armat"
+puts "hello \rnext"
+puts "H\111"
